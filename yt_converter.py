a618f3776001a064fb71211f9497cafe35385d85
DayHieN <sanblas.cerumen@gmail.com>
2024-02-09 18:00:26 +0100

    fix: made program more efficient by detecting if link is a list, fix: rearranged labels for better readability

--- yt_converter.py
+++ yt_converter.py
@@ -3,6 +3,7 @@ from pytube.exceptions import AgeRestrictedError
 import customtkinter as ctk
 import re
 import threading
+from time import sleep
 
 
 def replace_invalid_chars(filename):
@@ -10,68 +11,100 @@ def replace_invalid_chars(filename):
     return re.sub(invalid_chars_regex, ' ', filename)
 
 
+def convert(path, link):
+    if 'list' in link:
+        dl_playlist(path, link)
+    else:
+        dl_single(path, link)
+
+
 def dl_single(path, link):
-
     if mp3_checked.get() == 'on':
-
         video = YouTube(link)
         stream = video.streams.get_audio_only()
-        complete_label.configure(text=f'Downloading: "{stream.title}"...')
+        complete_label.configure(
+            text=f'Downloading: "{stream.title}"...', text_color='cyan')
         try:
             stream.download(output_path=path,
                             filename=f'{replace_invalid_chars(stream.title)}.mp3', timeout=20)
-        except AgeRestrictedError:
-            pass
-        complete_label.configure(
-            text=f'"{stream.title}" download complete.')
+            complete_label.configure(
+                text=f'"{stream.title}" download complete.', text_color='green')
+        except Exception as e:
+            complete_label.configure(
+                text=f"'{stream.title}' {e}.", text_color='orange')
 
     if mp4_checked.get() == 'on':
-
         video = YouTube(link)
         stream = video.streams.filter(progressive=True, res='720p').first()
-        complete_label.configure(text=f'Downloading: "{stream.title}"...')
-        stream.download(output_path=path,
-                        filename=f'{replace_invalid_chars(stream.title)}.mp4')
         complete_label.configure(
-            text=f'"{stream.title}" download complete.')
+            text=f'Downloading: "{stream.title}"...', text_color='cyan')
+        try:
+            stream.download(output_path=path,
+                            filename=f'{replace_invalid_chars(stream.title)}.mp4', timeout=20)
+            complete_label.configure(
+                text=f'"{stream.title}" download complete.', text_color='green')
+        except Exception as e:
+            complete_label.configure(
+                text=f'"{stream.title}" {e}.', text_color='orange')
 
 
 def dl_playlist(path, link):
-
     playlist = Playlist(link)
-
     if mp3_checked.get() == 'on':
         for i, video in enumerate(playlist.videos):
+            playlist_label.configure(
+                text=f'Downloading: {playlist.title} playlist')
             complete_label.configure(
-                text=f'Downloading: {playlist.title}... {video.title} {i+1} of {len(playlist.videos)}')
-            stream = video.streams.get_audio_only()
-            stream.download(output_path=f'{path}/{playlist.title}',
-                            filename=f'{replace_invalid_chars(stream.title)}.mp3')
+                text=video.title, text_color='cyan')
+            count_label.configure(text=f'( {i+1} / {len(playlist.videos)} )')
+            try:
+                stream = video.streams.get_audio_only()
+                stream.download(output_path=f'{path}/{playlist.title}',
+                                filename=f'{replace_invalid_chars(stream.title)}.mp3', timeout=20)
+            except Exception as e:
+                complete_label.configure(
+                    text=f'{playlist.title} {e}.', text_color='orange')
+                sleep(2)
+                continue
         complete_label.configure(
-            text=f'{playlist.title} playlist download complete.')
+            text=f'{playlist.title} playlist download complete.', text_color='green')
+        playlist_label.configure(text='')
+        count_label.configure(text='')
 
     if mp4_checked.get() == 'on':
         for i, video in enumerate(playlist.videos):
+            playlist_label.configure(
+                text=f'Downloading: {playlist.title} playlist')
             complete_label.configure(
-                text=f'Downloading: {playlist.title}... {video.title} {i+1} of {len(playlist.videos)}')
+                text=video.title, text_color='cyan')
+            count_label.configure(text=f'( {i+1} / {len(playlist.videos)} )')
             try:
                 stream = video.streams.filter(
                     progressive=True, res='720p').first()
             except:
                 stream = video.streams.filter(progressive=True).first()
-            stream.download(output_path=f'{path}/{playlist.title}',
-                            filename=f'{replace_invalid_chars(stream.title)}.mp4')
+            try:
+                stream.download(output_path=f'{path}/{playlist.title}',
+                                filename=f'{replace_invalid_chars(stream.title)}.mp4', timeout=20)
+            except Exception as e:
+                complete_label.configure(
+                    text=f'{playlist.title} {e}.', text_color='orange')
+                sleep(2)
+                continue
         complete_label.configure(
-            text=f'{playlist.title} playlist download complete.')
+            text=f'{playlist.title} playlist download complete.', text_color='green')
+        playlist_label.configure(text='')
+        count_label.configure(text='')
 
 
 def open_file_dialog():
     global file_path
     file_path = ctk.filedialog.askdirectory()
     if file_path:
-        output_label.configure(text=f'Download will go to: {file_path}')
+        output_label.configure(text=f'Download will be stored at: {file_path}')
 
 
+# tk window settings
 root = ctk.CTk()
 root.title('Youtube Converter')
 root.geometry('700x500')
@@ -90,7 +123,7 @@ youtube_link_entry.pack(pady=50)
 output_path_button = ctk.CTkButton(
     root, text='Output directory', command=open_file_dialog)
 output_path_button.pack()
-output_label = ctk.CTkLabel(root, text='')
+output_label = ctk.CTkLabel(root, text='', text_color='cyan')
 output_label.pack()
 
 mp3_checkbox = ctk.CTkCheckBox(
@@ -101,19 +134,30 @@ mp4_checkbox = ctk.CTkCheckBox(
     master=root, text='.mp4', variable=mp4_checked, onvalue='on', offvalue='off')
 mp4_checkbox.pack(pady=10)
 
+
 convert_single = ctk.CTkButton(
-    root, text='Convert single', command=lambda: threading.Thread(target=dl_single, args=(file_path, youtube_link_entry.get())).start())
+    root, text='Download', command=lambda: threading.Thread(target=convert, args=(file_path, youtube_link_entry.get())).start())
 convert_single.pack(pady=10)
 
-convert_playlist = ctk.CTkButton(
-    root, text='Convert playlist', command=lambda: threading.Thread(target=dl_playlist, args=(file_path, youtube_link_entry.get())).start())
-convert_playlist.pack(pady=10)
 
+# convert_single = ctk.CTkButton(
+#     root, text='Convert single', command=lambda: threading.Thread(target=dl_single, args=(file_path, youtube_link_entry.get())).start())
+# convert_single.pack(pady=10)
+
+# convert_playlist = ctk.CTkButton(
+#     root, text='Convert playlist', command=lambda: threading.Thread(target=dl_playlist, args=(file_path, youtube_link_entry.get())).start())
+# convert_playlist.pack(pady=10)
+
+playlist_label = ctk.CTkLabel(root, text='', wraplength=400, text_color='cyan')
+playlist_label.pack()
 
 complete_label = ctk.CTkLabel(root, text='', wraplength=400)
 complete_label.pack()
 
+count_label = ctk.CTkLabel(root, text='', text_color='cyan')
+count_label.pack()
 
+# center window on screen
 root.update_idletasks()  # Update geometry
 x = (root.winfo_screenwidth() - root.winfo_reqwidth()) // 2
 y = (root.winfo_screenheight() - root.winfo_reqheight()) // 2
